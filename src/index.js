90388dd67db0a35362a09f53b46b3bbe2a06cac4
Abhay Singh Thakur <thakur22429s@gmail.com>
2021-07-15 11:25:56 +0400

    Routing Fix Attempt #2

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
       <App />
-    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
